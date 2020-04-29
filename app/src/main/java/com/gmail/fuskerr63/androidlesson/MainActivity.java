95c50a229b541bc3ead6e05dbca65349cdc2ade9
Boldyrig <35888160+Boldyrig@users.noreply.github.com>
2020-04-29 13:06:39 +0400

    Add files via upload

--- app/src/main/java/com/gmail/fuskerr63/androidlesson/MainActivity.java
+++ app/src/main/java/com/gmail/fuskerr63/androidlesson/MainActivity.java
@@ -1,14 +1,38 @@
 package com.gmail.fuskerr63.androidlesson;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.gmail.fuskerr63.fragments.ContactDetailsFragment;
+import com.gmail.fuskerr63.fragments.ContactListFragment;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Fragment contactListFragment = ContactListFragment.newInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // добавление toolbar
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        // транзакция: добавить фрагмент списка конактов
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_container, contactListFragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_container, ContactDetailsFragment.newInstance(view.getId()));
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/gmail/fuskerr63/fragments/ContactDetailsFragment.java
@@ -0,0 +1,39 @@
+package com.gmail.fuskerr63.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gmail.fuskerr63.androidlesson.R;
+
+public class ContactDetailsFragment extends Fragment {
+    private static int id;
+
+    public ContactDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        TextView title = getActivity().findViewById(R.id.title);
+        title.setText("Contact Details");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact_details, container, false);
+    }
+
+    public static Fragment newInstance(int contactId) {
+        id = contactId;
+        Fragment contactDetails = new ContactDetailsFragment();
+        return contactDetails;
+    }
+}
